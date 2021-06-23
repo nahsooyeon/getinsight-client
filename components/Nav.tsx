05046d1f7c2e673e79959015e07e1e83ddcd6be5
nahsooyeon <nahsooyeon@gmail.com>
2021-06-23 17:47:49 +0900

    🔨 추가: 상단 네비게이터 컴포넌트 파일

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,13 +2,12 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const Nav = (props: any) => (
-	<div className="nav">
-		<Link href="/search">
-			<a></a>
-		</Link>
-
-		<Link href="/shopping"><a></a></Link>
-	</div>
+  <div className="nav">
+    <Link href="/search">
+      <a>키워드 검색 서비스</a>
+    </Link>
+    <Link href="/shopping"><a>쇼핑 트렌드 분석 서비스</a></Link>
+  </div>
 );
 
 
