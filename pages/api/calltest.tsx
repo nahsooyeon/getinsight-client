d57a7b484241aa92a9d22d0fcf97a727ce411401
dona Kim <windofwind@users.noreply.github.com>
2021-06-24 14:02:54 +0900

    Update calltest.tsx

--- pages/api/calltest.tsx
+++ pages/api/calltest.tsx
@@ -3,6 +3,14 @@ import Logger from "../../commons/Logger";
 
 
 export default async function handler(req, res) {
+  // https://github.com/winstonjs/winston#using-logging-levels 참고
+  Logger.log("info", "콜테스트 로그");
+
+  Logger.info("calltest info");
+  Logger.debug("calltest debug")
+  Logger.error("calltest error");
+  Logger.warn("calltest warn");
+
   let resData;
   if (req.method === 'POST') {
     resData = { success: true, timestamp: dayjs().unix(), timeFormat: dayjs().format("YYYY-MM-DD HH:mm:ss Z") };
