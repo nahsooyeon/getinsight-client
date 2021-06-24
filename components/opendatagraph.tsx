5170c066880b9acd5dc4c6340effba369d178f25
nahsooyeon <nahsooyeon@gmail.com>
2021-06-24 22:24:50 +0900

    🚚 수정: 컴포넌트 이름 수정

---
+++ components/opendatagraph.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import { KeywordResult } from '../interfaces/interfaces';
+import dynamic from 'next/dynamic';
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+
+interface Props {
+  resultData: KeywordResult;
+}
+
+function openDataGraph(props: Props): ReactElement {
+  const { resultData } = props;
+  const categoryArray = [];
+  resultData.results[0].data.forEach((el) => categoryArray.push(el.period));
+  const ratioArray = [];
+  resultData.results[0].data.forEach((el) => ratioArray.push(Math.round(el.ratio)));
+  const data = {
+    options: {
+      chart: {
+        zoom: {
+          enabled: false,
+        }
+      },
+      xaxis: {
+        categories: categoryArray,
+        tickAmount: 10,
+      },
+      yaxis: {
+        max: 100
+      },
+
+    },
+    series: [
+      {
+        name: resultData.results[0].title,
+        data: ratioArray
+      }
+    ]
+  };
+
+  return (
+    <div className="line-graph-view">그래프 구역입니다
+      <Chart options={data.options} series={data.series} type="line" width="500" />
+    </div>
+  );
+};
+
+export default openDataGraph;
