371db6ef47a9a0325399c382a3d33a84039cd90a
nahsooyeon <nahsooyeon@gmail.com>
2021-06-30 17:32:08 +0900

    🔨 수정: 검색광고 API 요청시 헤더 변수 선언 코드 수정

--- pages/api/adsearch.tsx
+++ pages/api/adsearch.tsx
@@ -1,32 +1,16 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
-// import { adKeywordBody } from "../../interfaces";
 import CryptoJS from "crypto-js";
 
-import Logger from "../../commons/Logger";
-import { log } from 'winston';
 
-const api_url = "/keywordstool";
-const method = "GET";
-const timestamp = Date.now() + '';
-const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, process.env.NAVER_SERACHAD_SECRET);
-hmac.update(timestamp + '.' + method + '.' + api_url);
-let hash = hmac.finalize();
-hash.toString(CryptoJS.enc.Base64);
-const headers = {
-  'X-timestamp': timestamp,
-  'X-API-KEY': process.env.NAVER_SEARCHAD_ACCESS,
-  'X-API-SECRET': process.env.NAVER_SERACHAD_SECRET,
-  'X-CUSTOMER': process.env.NAVER_SERACHAD_ID,
-  'X-Signature': hash.toString(CryptoJS.enc.Base64),
 
-};
+
 
 export default async function handler(req: Request, res: Response) {
   if (req.method === 'POST') {
     const result = await adKeywordSearch(req.body.keyword);
-    Logger.info('logger test');
     res.status(200).json(result.data);
+
   } else {
     res.status(403).json({ success: false });
   }
@@ -34,12 +18,28 @@ export default async function handler(req: Request, res: Response) {
 }
 
 const adKeywordSearch = async (data: string) => {
+  const api_url = "/keywordstool";
+  const method = "GET";
+  const timestamp = Date.now() + '';
+  const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, process.env.NAVER_SERACHAD_SECRET);
+  hmac.update(timestamp + '.' + method + '.' + api_url);
+  let hash = hmac.finalize();
+  hash.toString(CryptoJS.enc.Base64);
+  const headers = {
+    'X-timestamp': timestamp,
+    'X-API-KEY': process.env.NAVER_SEARCHAD_ACCESS,
+    'X-API-SECRET': process.env.NAVER_SERACHAD_SECRET,
+    'X-CUSTOMER': process.env.NAVER_SERACHAD_ID,
+    'X-Signature': hash.toString(CryptoJS.enc.Base64),
+
+  };
   let result;
   try {
     const response = await axios.get('https://api.naver.com/keywordstool?hintKeywords=' + encodeURI(data) + '&showDetail=1', {
       headers: headers,
     });
     result = response;
+
   } catch (error) {
     console.log(error);
   }
