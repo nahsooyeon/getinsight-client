457ae82b7753a4163fd80296c5470a000e39da68
nahsooyeon <nahsooyeon@gmail.com>
2021-07-05 14:32:14 +0900

    🔇 삭제: 콘솔로그

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -7,7 +7,6 @@ export default async function handler(req: Request, res: Response) {
   try {
     if (req.method === 'POST') {
       const result = await datalabSearch(req.body);
-      console.log(result);
       res.status(200).send({ result });
     } else {
       res.status(403).json({ success: false });
