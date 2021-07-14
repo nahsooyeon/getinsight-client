b6c7a987e08bdda606d1ceff3a377ccdbdb8779c
nahsooyeon <nahsooyeon@gmail.com>
2021-07-14 15:25:44 +0900

    🔨 수정: 최상단 네비게이터 컴포넌트 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { ReactElement } from 'react';
+import MainNav from "../components/mainnav";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const styles = require('../styles/Home.module.scss');
@@ -15,25 +16,18 @@ export default function Home(): ReactElement {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <MainNav />
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="/">Get-Insight</a>
-        </h1>
+        <h3 className={styles.title}>
+          <h3>Simplify your Business with us!</h3>        </h3>
+
+        <div className="group-index-btn"> <button className="btn-trial">Start free trial</button>
+          <button className="btn-introduction">Learn more</button></div>
+
+
 
         <div className={styles.grid}>
-          <Link href="/search">
-            <a className={styles.card}>
-              <h2>키워드 검색량 분석 &rarr;</h2>
-              <p>특정 키워드의 일간/주간/월간 검색량 추이를 분석하세요</p>
-            </a>
-          </Link>
-
-          <Link href="/shopping">
-            <a className={styles.card}>
-              <h2>쇼핑 트렌드 분석 &rarr;</h2>
-              <p>특정 카테고리의 인기검색어와 검색 트렌드를 분석하세요</p>
-            </a>
-          </Link>
 
         </div>
       </main>
